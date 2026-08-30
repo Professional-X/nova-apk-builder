5cce93ad5b75220201f4ee63265a7b6206f7c927
NovaBot <nova@builder.dev>
2026-08-30 12:10:22 +0000

    Fix config: restore correct OAuth Client ID

--- site/js/config.js
+++ site/js/config.js
@@ -15,7 +15,7 @@
 const CONFIG = {
   GITHUB_OWNER: 'Professional-X',
   GITHUB_REPO:  'nova-apk-builder',
-  OAUTH_CLIENT_ID: '08028a69ee89d0a8814f2a8ec9148d555f54f96e',   // <-- Paste your OAuth App Client ID here
+  OAUTH_CLIENT_ID: 'Ov23110ErDURA011z3x5',   // <-- Your OAuth App Client ID
   API_BASE: 'https://api.github.com',
   MAX_ZIP_MB: 100,
   POLL_INTERVAL_MS: 8000,
